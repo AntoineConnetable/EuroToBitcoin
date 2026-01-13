70c2573dec4f5a352774e68f60389bf6dfdebe41
Morphee1337 <arthur.israel@edouardgand.fr>
2026-01-13 11:22:10 +0100

    Version Corrigé

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function convertir() {
-    let euro document.getElementByid("euro").value;
+    let euro = document.getElementById("euro").value;
 
     // Taux au 10/01/2026	1 BTC  77 600 €
-    let taux 77600;
+    let taux = 77600;
 
     // BUG : la formule est incorrecte let bitcoin = euro * taux;
+    let bitcoin = euro * taux;
 
-    document.getElementByid("resultat").textContent euro + "€ = "+ bitcoin +" BTC";
+    document.getElementById("resultat").textContent = euro + "€ = "+ bitcoin +" BTC";
 }
